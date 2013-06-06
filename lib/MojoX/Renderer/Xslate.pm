24dcc6c8ca4291f9ef7467ff7d0910bd3e5e8b56
gray <gray@cpan.org>
2013-06-05 20:31:16 -0700

    update copyright dates

--- lib/MojoX/Renderer/Xslate.pm
+++ lib/MojoX/Renderer/Xslate.pm
@@ -167,7 +167,7 @@ L<http://search.cpan.org/dist/MojoX-Renderer-Xslate/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010-2012 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2010-2013 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
