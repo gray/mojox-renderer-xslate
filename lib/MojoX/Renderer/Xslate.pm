59c4d0610354a7b9c201d12390e356e2cabb8fdd
Heiko Jansen <heiko_jansen@web.de>
2018-10-07 20:44:57 +0200

    Prepare next release v0.14

--- lib/MojoX/Renderer/Xslate.pm
+++ lib/MojoX/Renderer/Xslate.pm
@@ -8,7 +8,7 @@ use Mojo::Base -base;
 use Mojo::Loader qw(data_section);
 use Text::Xslate ();
 
-our $VERSION = '0.13';
+our $VERSION = '0.14';
 $VERSION = eval $VERSION;
 
 has 'xslate';
@@ -190,7 +190,7 @@ L<http://search.cpan.org/dist/MojoX-Renderer-Xslate/>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2010-2016 gray <gray at cpan.org>, all rights reserved.
+Copyright (C) 2010-2018 gray <gray at cpan.org>, all rights reserved.
 
 This library is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
